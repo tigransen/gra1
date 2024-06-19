e659d997d833751e4262f17bc1f5793862d14b63
jaceksen <jaceksen@gmail.com>
2024-06-19 12:03:35 +0200

    objects

--- plik1.js
+++ plik1.js
@@ -1,1 +1,16 @@
-console.log("32123")
+console.log("32123");
+
+
+let jacek = {
+    imie: "Jacek",
+    nazwisko: "Seń",
+    lat: 55
+}
+
+console.log(jacek.imie);
+
+console.log(jacek.lat);
+
+let miasto = "Gdyni";
+
+console.log(`Mieszkam w ${miasto} i mam na imię ${jacek.imie} ${jacek.nazwisko} i mam ${jacek.lat} lat`)
